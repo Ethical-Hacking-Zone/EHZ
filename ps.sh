e91205ae9ca6fa62a3290b3e310770fa57b5fec5
Ethical-Hacking-Zone <55155544+Ethical-Hacking-Zone@users.noreply.github.com>
2020-06-13 11:52:58 +0800

    Update ps.sh

--- ps.sh
+++ ps.sh
@@ -1,12 +1,17 @@
+git clone https://github.com/MasterDevX/Termux-ADB
+cd Termux-ADB
+chmod +x InstallTools.sh
+./InstallTools.sh
+clear
 pkg update -y
 pkg upgrade -y 
 pkg install git -y
 pkg install python2 -y
 pkg install python -y
-git clone https://github.com/metachar/PhoneSploit/
+git clone https://github.com/metachar/PhoneSploit
 cd PhoneSploit
+chmod +x main_linux.py
+pip2 install colorama
+pip2 install lolcat
 pkg install toilet -y
-pip install lolcat
-pip install  colorama
-apt update > /dev/null 2>&1 && apt --assume-yes install wget > /dev/null 2>&1 && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh -q && bash InstallTools.sh
 python2 main_linux.py
